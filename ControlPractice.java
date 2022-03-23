b0ad95cde1eddee52e809e48641e4db605fb625e
Himalware <malwarehijack@gmail.com>
2022-03-24 00:14:33 +0530

    New project to git

---
+++ ControlPractice.java
@@ -0,0 +1,17 @@
+
+public class ControlPractice {
+
+	public static void main(String[] args) {
+
+		int i = 17;
+
+		if (i > 16) {
+			System.out.println(" ALLOWED");
+		} else if (i == 10) {
+			System.out.println("good");
+		} else {
+			System.out.println("n/a");
+		}
+	}
+
+}
---
+++ SwitchClass.java
@@ -0,0 +1,31 @@
+
+public class SwitchClass {
+
+	public static void main(String[] args) {
+
+		int i = 10;
+		switch (i = 12) {
+		case 1:
+			System.out.println("ONE");
+			break;
+		case 2:
+			System.out.println("two");
+			break;
+		case 3:
+			System.out.println("three");
+			break;
+		case 4:
+			System.out.println("four");
+			break;
+		case 5:
+			System.out.println("five");
+			break;
+		case 6:
+			System.out.println("six");
+			break;
+		default:
+			System.out.println("end");
+			break;
+		}
+	}
+}
